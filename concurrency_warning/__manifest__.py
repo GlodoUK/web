7bc988ab1e12597c0e936103d6543db9650f5102
Karl Southern <karl@theangryangel.co.uk>
2020-10-28 12:15:11 +0000

    Fix common issues. Remove unused modules. Update README in preparation for open sourcing

--- concurrency_warning/__manifest__.py
+++ concurrency_warning/__manifest__.py
@@ -1,15 +1,20 @@
-# -*- coding: utf-8 -*-
 {
     "name": "concurrency_warning",
     "summary": """Issue a concurrency warning if a user has left a model
     open, and it been altered in the meantime.""",
     "description": """Issue a concurrency warning if a user has left a model
     open, and it been altered in the meantime.""",
-    "author": "Glo Networks",
-    "website": "https://www.glo.systems/",
+    "author": "Glodo",
+    "website": "https://www.glodo.uk/",
     "category": "Uncategorized",
-    "version": "1.0.0",
-    "depends": ["base", "base_automation", "sale_management", "purchase"],
+    "version": "12.0.1.0.0",
+    "depends": [
+        "base",
+        "base_automation",
+        "sale_management",
+        "purchase"
+    ],
     "data": ["data/base_automation.xml", "views/templates.xml"],
     "demo": [],
+    "license": "LGPL-3",
 }
