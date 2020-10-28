e3467d3c4166799d1c743a31c1f019a781698586
Karl Southern <karl@theangryangel.co.uk>
2020-10-28 15:38:19 +0000

    Fix pre-commit. Add web_leaflet

---
+++ web_leaflet/README.rst
@@ -0,0 +1,5 @@
+===========
+web_leaflet
+===========
+
+Creates a leaflet.js powered controller and view that renders a map.
---
+++ web_leaflet/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "web_leaflet",
+    "summary": "Defines an Odoo Enterprise-like Map view",
+    "development_status": "Alpha",
+    "category": "Hidden",
+    "version": "13.0.1.0.0",
+    "depends": ["web", "base_setup"],
+    "data": ["views/assets.xml"],
+    "qweb": ["static/xml/templates.xml"],
+    "auto_install": True,
+    "author": "Glodo",
+    "website": "https://www.glodo.uk/",
+    "license": "LGPL-3",
+}
