eed7555df8f57ce4754f886b5ef0f035b1ab5bb0
Matt Lipski <mattlipski5543@gmail.com>
2020-12-31 10:18:24 +0000

    last update for manifest...

--- concurrency_warning/__manifest__.py
+++ concurrency_warning/__manifest__.py
@@ -1,13 +1,15 @@
 {
     "name": "concurrency_warning",
-    "summary": """Issue a concurrency warning if a user has left a model
+    "summary": """Issue a concurrency warning and reload
+    the page content if a user has left a model
     open, and it been altered in the meantime.""",
-    "description": """Issue a concurrency warning if a user has left a model
+    "description": """Issue a concurrency warning and reload
+    the page content if a user has left a model
     open, and it been altered in the meantime.""",
     "author": "Glodo",
     "website": "https://www.glodo.uk/",
     "category": "Uncategorized",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "depends": [
         "base",
         "base_automation",
