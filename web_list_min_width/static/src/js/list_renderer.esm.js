fcfa697a75c234fbe4632d2fa0617208d2ab391a
Karl <karl@theangryangel.co.uk>
2023-10-10 19:37:22 +0100

    Update list_renderer.esm.js

--- web_list_min_width/static/src/js/list_renderer.esm.js
+++ web_list_min_width/static/src/js/list_renderer.esm.js
@@ -5,6 +5,7 @@ import {patch} from "@web/core/utils/patch";
 
 patch(ListRenderer.prototype, "web_list_min_width", {
     calculateColumnWidth(column) {
+        // backwards compat
         if (
             column.rawAttrs !== undefined &&
             column.rawAttrs["min-width"] !== undefined
@@ -14,6 +15,17 @@ patch(ListRenderer.prototype, "web_list_min_width", {
                 value: column.rawAttrs["min-width"],
             };
         }
+
+        if (
+            column.options !== undefined &&
+            column.options['min-width'] !== undefined
+        ) {
+            return {
+                type: "absolute",
+                value: column.options["min-width"],
+            };
+        }
+
         return this._super(column);
     },
 });
